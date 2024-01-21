413e5748ebaa3ff62e24260a712f148426493035
n-y-kim <nyn2265@gmail.com>
2024-01-21 16:11:30 +0900

    Add ds Dockerfile & Update manifests

--- loggingconfig/logs.py
+++ loggingconfig/logs.py
@@ -2,7 +2,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
-    format='%(asctime)s|%(levelname)s] - %(message)s',
+    format='[%(asctime)s|%(levelname)s] - %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S'
 )
